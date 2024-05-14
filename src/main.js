44345209c73cbd4436e0352088817969de073371
hao.zhaoyu <hzy_0228@163.com>
2024-05-15 00:33:42 +0800

    Vue 整合 Axios 实现登录功能（解决跨域问题）, 登录页表单验证

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import "@/assets/main.css";
-
 import { createApp } from "vue";
 import App from "@/App.vue";
 // 导入路由
 import router from "@/router";
+// 导入 Element Plus 图标
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 const app = createApp(App);
 
 // 应用路由
 app.use(router);
 app.mount("#app");
+
+// 引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
