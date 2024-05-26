d7fb2594b135cd6841903eda576257291142bba8
hao.zhaoyu <hzy_0228@163.com>
2024-05-26 14:40:30 +0800

    全局路由拦截：实现页面标题动态设置、后台路由跳转的登录判断

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import "@/assets/main.css";
+import "nprogress/nprogress.css";
 import { createApp } from "vue";
+
 import App from "@/App.vue";
-// 导入路由
-import router from "@/router";
-// 导入 Element Plus 图标
-import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 // 导入路由
 import router from "@/router";
 // 导入全局路由守卫
 import "@/permission";
+// 导入 Element Plus 图标
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 const app = createApp(App);
 
