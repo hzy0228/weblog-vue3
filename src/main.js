da2ea4d5863172ecee9b9de30754480b3b0d0fb7
hao.zhaoyu <hzy_0228@163.com>
2024-06-02 23:31:31 +0800

    Element Plus 手搭 Admin 管理后台骨架

--- src/main.js
+++ src/main.js
@@ -10,10 +10,9 @@ import "@/permission";
 // 导入 Element Plus 图标
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 // 引入全局状态管理 Pinia
-import { createPinia } from "pinia";
+import pinia from "@/stores";
 
 const app = createApp(App);
-const pinia = createPinia();
 
 // 应用路由
 app.use(router);
--- src/pages/admin/login.vue
+++ src/pages/admin/login.vue
@@ -51,10 +51,12 @@
 import { User, Lock } from '@element-plus/icons-vue'
 import { login } from '@/api/admin/user'
 import { ref, reactive, onMounted, onBeforeUnmount } from 'vue'
-import { setToken } from '@/composables/auth'
+import { setToken } from '@/composables/cookie'
 import { useRouter } from 'vue-router'
 import { showMessage } from '@/composables/util'
+import { useUserStore } from '@/stores/user'
 
+const userStore = useUserStore()
 
 const router = useRouter()
 // 登录按钮加载
@@ -113,6 +115,9 @@ const onSubmit = () => {
                 // 存储 Token 到 Cookie 中
                 let token = res.data.token
                 setToken(token)
+
+                // 获取用户信息，并存储到全局状态中
+                userStore.setUserInfo()
 
                 // 跳转到后台首页
                 router.push('/admin/index')
