4528c0544664301f6f61f81bd80a2436910a06de
hao.zhaoyu <hzy_0228@163.com>
2025-03-25 15:42:19 +0800

    使用 Vue 3 + Vite 4 搭建前端工程

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import '@/assets/main.css'
+
+import { createApp } from 'vue'
+import App from '@/App.vue'
+// 导入路由
+import router from '@/router'
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
