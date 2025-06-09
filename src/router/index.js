6618769b362e7288e9e2b201a3c5041370d28e83
hao.zhaoyu <hzy_0228@163.com>
2025-06-09 10:19:44 +0800

    归档页、分类列表页、标签列表页开发

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,12 @@ import AdminArticleList from '@/pages/admin/article-list.vue'
 import AdminCategoryList from '@/pages/admin/category-list.vue'
 import AdminTagList from '@/pages/admin/tag-list.vue'
 import AdminBlogSetting from '@/pages/admin/blog-settings.vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import ArchiveList from '@/pages/frontend/archive-list.vue'
+import CategoryList from '@/pages/frontend/category-list.vue'
+import CategoryArticleList from '@/pages/frontend/category-article-list.vue'
+import TagList from '@/pages/frontend/tag-list.vue'
+import TagArticleList from '@/pages/frontend/tag-article-list.vue'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
 // 统一在这里声明所有路由
 const routes = [
@@ -80,7 +85,42 @@ const routes = [
             },
         ]
 
-    }
+    },
+    {
+        path: '/archive/list', // 归档页
+        component: ArchiveList,
+        meta: {
+            title: 'Weblog 归档页'
+        }
+    },
+    {
+        path: '/category/list', // 分类页
+        component: CategoryList,
+        meta: { // meta 信息
+            title: 'Weblog 分类页'
+        }
+    },
+    {
+        path: '/category/article/list', // 分类文章页
+        component: CategoryArticleList,
+        meta: { // meta 信息
+            title: 'Weblog 分类文章页'
+        }
+    },
+    {
+        path: '/tag/list', // 标签列表页
+        component: TagList,
+        meta: { // meta 信息
+            title: 'Weblog 标签列表页'
+        }
+    },
+    {
+        path: '/tag/article/list', // 标签列表页
+        component: TagArticleList,
+        meta: { // meta 信息
+            title: 'Weblog 标签文章页'
+        }
+    },
 ]
 
 // 创建路由
